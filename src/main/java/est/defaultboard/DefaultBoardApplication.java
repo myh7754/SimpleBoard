019a1920edcc7b58da923dd2f290b0db74e5fc45
myh7754 <myh4755@gmail.com>
2025-02-20 04:34:13 +0900

    feat(security) : login 기능추가

---
+++ src/main/java/est/defaultboard/DefaultBoardApplication.java
@@ -0,0 +1,13 @@
+package est.defaultboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/est/defaultboard/DefaultBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package est.defaultboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
